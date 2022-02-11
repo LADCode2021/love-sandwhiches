e1f1664a072aaa4add4c391518f4c9d5ec3556b1
LADCode2021 <luke.dawson@live.co.uk>
2022-02-11 14:17:22 +0000

    Ass user request loop to repeat request for data until valid data is provided

--- run.py
+++ run.py
@@ -9,31 +9,36 @@ SCOPE = [
 
 CREDS = Credentials.from_service_account_file('creds.json')
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
-GSPSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
-SHEET = GSPSPREAD_CLIENT.open('love_sandwiches')
+GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+SHEET = GSPREAD_CLIENT.open('love_sandwiches')
+
 
 def get_sales_data():
     """
-    Get sales figures input from user
+    Get sales figures input from the user.
     """
+    while True:
+        print("Please enter sales data from the last market.")
+        print("Data should be six numbers, separated by commas.")
+        print("Example: 10,20,30,40,50,60\n")
 
-    print("Please enter sales data from last market")
-    print("Data should be six numbers, separated by commas")
-    print("Example: 10, 20, 30, 40, 50, 60\n")
+        data_str = input("Enter your data here: ")
 
-    data_str = input("Enter your data here: ")
+        sales_data = data_str.split(",")
 
-    sales_data = data_str.split(",")
-    validate_data(sales_data)
+        if validate_data(sales_data):
+            print("Data is valid!")
+            break
+
+    return sales_data
 
 
 def validate_data(values):
     """
-    Inside the try, converts all string values into integers. 
-    Raises ValueError if strings cannot be into int, 
-    or if there aren't exactly 6 values'
+    Inside the try, converts all string values into integers.
+    Raises ValueError if strings cannot be converted into int,
+    or if there aren't exactly 6 values.
     """
-    print(values)
     try:
         [int(value) for value in values]
         if len(values) != 6:
@@ -42,8 +47,9 @@ def validate_data(values):
             )
     except ValueError as e:
         print(f"Invalid data: {e}, please try again.\n")
+        return False
+
+    return True
 
 
-get_sales_data()
-
-
+data = get_sales_data()
